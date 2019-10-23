7b5cf4a7a40d5ea4b1a73aede954bf604d6d1d4e
Rascalov <tim.sanou22@gmail.com>
2019-10-23 12:54:28 +0200

    quick fix to check RomeTW app exists

--- RomeShellLaziness/Form1.cs
+++ RomeShellLaziness/Form1.cs
@@ -40,28 +40,25 @@ namespace RomeShellLaziness
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string command = "give_trait";
-            string Name = cmbName.Text + " ";
-            string SurName = cmbSurName.Text;
-            string traitWhole = cmbTrait.Text;
-            string TotalName = "\"" + string.Concat(Name, SurName) + "\"" ;
-          
-            string[] traitsplit = traitWhole.Split(' ');
-
-
-            string traitName = "\"" + traitsplit[0] + "\"";
-            string traitLevel = traitsplit[1];
-
-
-            //string fullCommand = command + " " + "\"" + TotalName + "\""  + " " + traitName + " " + traitLevel  ;
             if (RomeTWWindowSwitch())
             {
+                string command = "give_trait";
+                string Name = cmbName.Text + " ";
+                string SurName = cmbSurName.Text;
+                string traitWhole = cmbTrait.Text;
+                string TotalName = "\"" + string.Concat(Name, SurName) + "\"";
+
+                string[] traitsplit = traitWhole.Split(' ');
+
+
+                string traitName = "\"" + traitsplit[0] + "\"";
+                string traitLevel = traitsplit[1];
                 sim.Keyboard.TextEntry(command + " " + TotalName + " " + traitName + " " + traitLevel);
                 sim.Keyboard.KeyPress(VirtualKeyCode.RETURN);
             }
             else
             {
-                lblLog.Text = "Rome total war not detected";
+                MessageBox.Show("Rome total war not detected");
             }
         }
 
@@ -73,22 +70,26 @@ namespace RomeShellLaziness
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            string command = "give_trait";
+            if (RomeTWWindowSwitch())
+            {
+                string command = "give_trait";
 
-            string Name = cmbName.Text;
-            string SurName = " " + cmbSurName.Text;
-            string[] GoodTraits = {"GoodCommander 5", "NaturalMilitarySkill 4", "RhetoricSkill 3"
+                string Name = cmbName.Text;
+                string SurName = " " + cmbSurName.Text;
+                string[] GoodTraits = {"GoodCommander 5", "NaturalMilitarySkill 4", "RhetoricSkill 3"
                                  , "NaturalPhilosophy 3","PlainRomanVirtue 3","PoliticsSkill 3","PhlegmHumour 3"
                                  , "RacesHaterRomanVice 3", "Intelligent 3", "HighPersonalSecurity 3", "GoodSiegeAttacker 5"
                                  , "GoodSiegeDefender 5", "Austere 3", "CounterSpy 4", "WellConnectedWife 3", "GoodAdministrator 5", "Handsome 3"
                                  , "AssassinMaster 4", "SpyMaster 5", "HaleAndHearty 3"};
-            for (int i = 0; i < GoodTraits.Length; i++)
-            {
-                giveTrait(command, Name, SurName, GoodTraits[i]);
+                for (int i = 0; i < GoodTraits.Length; i++)
+                {
+                    giveTrait(command, Name, SurName, GoodTraits[i]);
+                }
             }
-
-            
-
+            else
+            {
+                MessageBox.Show("Rome total war not detected");
+            }
         }
 
         void giveTrait(string command, string Name, string SurName, string traitWhole)
@@ -114,45 +115,52 @@ namespace RomeShellLaziness
             string traitName = "\"" + traitsplit[0] + "\"";
             string traitLevel = traitsplit[1];
 
+            sim.Keyboard.KeyPress(VirtualKeyCode.OEM_3);
+            sim.Keyboard.TextEntry(command + " " + TotalName + " " + traitName + " " + traitLevel);
 
-            if (RomeTWWindowSwitch())
-            {
-                sim.Keyboard.KeyPress(VirtualKeyCode.OEM_3);
-                sim.Keyboard.TextEntry(command + " " + TotalName + " " + traitName + " " + traitLevel);
-
-                sim.Keyboard.KeyPress(VirtualKeyCode.RETURN);
-            }
-            else 
-            {
-                MessageBox.Show("Rome total war not detected");
-            }
+            sim.Keyboard.KeyPress(VirtualKeyCode.RETURN);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            string command = "give_trait";
-
-            string Name = cmbName.Text;
-            string SurName =" " + cmbSurName.Text;
-            string[] BadTraits = { "BadAdministrator 4", "BadAttacker 5", "BadBuilder 3", "Cuckold 4", "LostEagle 2"
-                                 , "Drink 6", "ExpensiveTastes 3", "Hypochondriac 3", "Inbred 6", "IanR 4"
-                                 , "Perverted 6", "PublicAtheism 6", "Xenophilia 4" };
-            for (int i = 0; i < BadTraits.Length; i++)
+            if (RomeTWWindowSwitch())
             {
-                giveTrait(command, Name, SurName, BadTraits[i]);
+                string command = "give_trait";
+
+                string Name = cmbName.Text;
+                string SurName = " " + cmbSurName.Text;
+                string[] BadTraits = { "BadAdministrator 4", "BadAttacker 5", "BadBuilder 3", "Cuckold 4", "LostEagle 2"
+                                     , "Drink 6", "ExpensiveTastes 3", "Hypochondriac 3", "Inbred 6", "IanR 4"
+                                     , "Perverted 6", "PublicAtheism 6", "Xenophilia 4" };
+                for (int i = 0; i < BadTraits.Length; i++)
+                {
+                    giveTrait(command, Name, SurName, BadTraits[i]);
+                }
             }
+            else
+            {
+                MessageBox.Show("Rome total war not detected");
+            }
+
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            string command = "give_trait";
-
-            string Name = cmbName.Text + " ";
-            string SurName = cmbSurName.Text;
-            string[] BadTraits = {"GoodAssassin 5", "GoodSpy 5" };
-            for (int i = 0; i < BadTraits.Length; i++)
+            if (RomeTWWindowSwitch())
             {
-                giveTrait(command, Name, SurName, BadTraits[i]);
+                string command = "give_trait";
+
+                string Name = cmbName.Text + " ";
+                string SurName = cmbSurName.Text;
+                string[] BadTraits = { "GoodAssassin 5", "GoodSpy 5" };
+                for (int i = 0; i < BadTraits.Length; i++)
+                {
+                    giveTrait(command, Name, SurName, BadTraits[i]);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Rome total war not detected");
             }
         }
 
